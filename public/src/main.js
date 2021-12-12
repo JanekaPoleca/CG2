15ef606f7eef5676b503ea459586ad51d9188a4a
Denomycor <afonsoresteves@hotmail.com>
2021-12-12 16:16:28 +0000

    Added commet and rings

--- public/src/main.js
+++ public/src/main.js
@@ -1,5 +1,6 @@
 import * as twgl from '../lib/twgl-full'
 import Sphere from './visualObjects/sphere'
+import Torus from './visualObjects/torus'
 import Universe from './visualObjects/universe'
 
 const {m4} = twgl
@@ -39,10 +40,10 @@ let main = () => {
     p3.addTranslation(72,0,0);
     p3.addRotationZ(0.007);
 
-    let m1 = new Sphere(gl, 2, 10, p3);
-    m1.addRotationZ(0.04);
-    m1.addTranslation(12,0,0);
-    m1.addRotationZ(0.06);
+    let s1 = new Sphere(gl, 2, 10, p3);
+    s1.addRotationZ(0.04);
+    s1.addTranslation(12,0,0);
+    s1.addRotationZ(0.06);
     
     let p4 = new Sphere(gl, 12, 25, universe);
     p4.addRotationZ(0.1);
@@ -54,27 +55,41 @@ let main = () => {
     p5.addTranslation(150,0,0);
     p5.addRotationZ(0.012);
 
-    let m2 = new Sphere(gl, 2, 10, p5);
-    m2.addRotationZ(0.1);
-    m2.addTranslation(13,0,0);
-    m2.addRotationZ(0.1);
+    let s2 = new Sphere(gl, 2, 10, p5);
+    s2.addRotationZ(0.1);
+    s2.addTranslation(13,0,0);
+    s2.addRotationZ(0.1);
 
-    let m3 = new Sphere(gl, 4, 20, p5);
-    m3.addRotationZ(0.2);
-    m3.addTranslation(25,0,0);
-    m3.addRotationZ(0.07);
+    let s3 = new Sphere(gl, 4, 20, p5);
+    s3.addRotationZ(0.2);
+    s3.addTranslation(25,0,0);
+    s3.addRotationZ(0.07);
 
-    let m4 = new Sphere(gl, 1, 8, m3);
-    m4.addRotationZ(0.5);
-    m4.addTranslation(6,0,0);
-    m4.addRotationZ(0.5);
+    let s4 = new Sphere(gl, 1, 8, s3);
+    s4.addRotationZ(0.5);
+    s4.addTranslation(6,0,0);
+    s4.addRotationZ(0.5);
 
     let p6 = new Sphere(gl, 20, 25, universe);
     p6.addRotationZ(0.002);
     p6.addTranslation(210,0,0);
     p6.addRotationZ(0.002);
 
-    let planets = [universe, star, p1, p2, p3, m1, p4, p5, m2, m3, m4, p6];
+    let r1 = new Torus(gl, 25, 3, 20, 20, p6);
+    r1.addRotationX(80, true);
+
+    let c1 = new Sphere(gl, 2, 20, universe);
+    c1.addTranslation(100, 0, 0);
+    c1.addEllipseZ(200, 50, -0.0005);
+
+    let planets = [star, p1, p2, p3, s1, p4, p5, s2, s3, s4, p6, r1, c1];
+
+
+
+    const update = time => {
+        universe.update(time)
+        planets.forEach(p => p.update(time))
+    }
 
     const render = time => {
         planets.forEach(p => p.update(time))
